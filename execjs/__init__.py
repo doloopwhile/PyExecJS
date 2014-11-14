6cc44759f06b950953770aa3de30c3f5b59a9ebf
OMOTO Kenji <doloopwhile@gmail.com>
2014-11-14 23:23:38 +0900

    Restore support for Python 2.6

--- execjs/__init__.py
+++ execjs/__init__.py
@@ -327,7 +327,7 @@ def encode_unicode_codepoints(str):
     >>> encode_unicode_codepoints('\u4e16\u754c') == '\\u4e16\\u754c'
     True
     """
-    codepoint_format = '\\u{:04x}'.format
+    codepoint_format = '\\u{0:04x}'.format
 
     def codepoint(m):
         return codepoint_format(ord(m.group(0)))
